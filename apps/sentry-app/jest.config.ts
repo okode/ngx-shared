61a8619351996e93d39d389e714a9c0750a96c95
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2023-04-04 10:20:24 +0200

    feat: rename sentry app

--- apps/sentry-app/jest.config.ts
+++ apps/sentry-app/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'ngx-shared-app',
+  displayName: 'sentry-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/ngx-shared-app',
+  coverageDirectory: '../../coverage/apps/sentry-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
